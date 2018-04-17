7b979ef65e4810ac55bae7ca70349c2a3b943fd5
davidgraf <david@graf-at.eu>
2018-04-17 15:53:33 +0200

    initial structure created

---
+++ iodata/readData.py
@@ -0,0 +1,16 @@
+'''
+Created: 17.04.2018
+@author:
+description:
+parameter:
+'''
+
+def readData(path):
+
+    # ... read features and labels from files and return data structure (already split to 4-folds?)
+
+    return None
+
+
+
+
---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+Created: 17.04.2018
+@author: davidgraf
+description: main application to run
+parameter:
+'''
+
+DATA_DIR = "C:/Temp/DCASE2017_development_set"
+
+# imports
+from iodata.readData import readData
+
+# read data
+data = readData(DATA_DIR)
+
+# data analysis
+# ... analysis(data)
+
+# preprocssing (feature scaling, feature evaluation, feature selection)
+# ...
+
+# training
+# ...
+
+# testing
+# ...
+
+# evaluating
+# ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
