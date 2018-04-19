4e6c070deb9b36ec4051797dfac550b4fbf99319
FlorianKiesl <f.kiesl@atn.nu>
2018-04-19 14:05:30 +0200

    Test

--- iodata/readData.py
+++ iodata/readData.py
@@ -5,10 +5,18 @@ description:
 parameter:
 '''
 
+import numpy as np
+import sklearn as sk
+
+file_path = '/Users/Florian/Documents/DCASE2017_development_set/mfcc_numpy'
+
 def readData(path):
 
     # ... read features and labels from files and return data structure (already split to 4-folds?)
-
+    data = np.load(file_path & '/mfcc_numpy/a001_0_10.npy')
+    print(data)
+    data1 = np.load('/Volumes/GoogleDrive/My Drive/WinStudium/MSc/ Machine Learning and Pattern Classification/DCASE2017_development_set/mfcc_numpy/a001_10_20.npy')
+    print(data1)
     return None
 
 
