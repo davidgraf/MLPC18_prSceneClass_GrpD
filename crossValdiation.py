d1e251d1edf4737140586c62a10b6b4d76e1f1ce
Martin <martinschobesberger@hotmail.com>
2018-05-02 12:32:36 +0200

    cross validation
    created cross validation as a standalone file. training doesn't work yet b/c there's a problem with mergen the read matrizes

---
+++ crossValdiation.py
@@ -0,0 +1,67 @@
+from iodata.readData import readFold
+import numpy as np
+from learning.classification import trainModel, testModel
+from processing.featureEvaluation import featureClassCoerr
+from processing.featureScaling import featureScale
+
+folds = []
+
+SAMPLERATE = 0.01
+overallAccuracy = 0
+
+
+class Fold:
+    features = np.array(None)
+    labels = np.array(None)
+
+class FoldData:
+    labels_train = None
+    feature_matrix_train = None
+
+#read the folds and store them for all iterations
+
+
+folds = [Fold() for j in range (4)]
+folds[0].features, folds[0].labels = readFold("fold1", "train", SAMPLERATE)
+folds[1].features, folds[1].labels = readFold("fold2", "train", SAMPLERATE)
+folds[2].features, folds[2].labels = readFold("fold3", "train", SAMPLERATE)
+folds[3].features, folds[3].labels = readFold("fold4", "evaluate", SAMPLERATE)
+
+
+for i in range(4):              #4 folds
+
+    trainData = [FoldData() for j in range (3)]
+
+    #3 training folds
+
+    trainData[0].feature_matrix_train = featureScale(folds[i].features)
+    trainData[1].labels_train= folds[i].labels.ravel()
+    trainData[1].feature_matrix_train = featureScale(folds[(i+1)%4].features)
+    trainData[1].labels_train = folds[(i+1)%4].labels.ravel()
+    trainData[2].feature_matrix_train = featureScale(folds[(i+2)%4].features)
+    trainData[2].labels_train = folds[(i+2)%4].labels.ravel()
+
+    featureMatrixTrain = np.append(trainData[0].feature_matrix_train, trainData[1].feature_matrix_train)
+    featureMatrixTrain = np.append(featureMatrixTrain, trainData[2].feature_matrix_train)
+
+    labelsTrain = np.append(trainData[0].labels_train, trainData[1].labels_train)
+    labelsTrain = np.append(labelsTrain, trainData[2].labels_train)
+
+    #1 evaluation fold
+
+    e_features = folds[(i+3)%4].features
+    e_labels = folds[(i+3)%4].labels
+
+    featureMatrixTest = featureScale(e_features)
+    labelsTest = e_labels.ravel()
+
+    #featureClassCoerr(featureMatrixTrain, labelsTrain, range(0,60))
+
+# training
+    model, meanCrossVal = trainModel(featureMatrixTrain, labelsTrain, 'SVM')
+
+# testing
+    accuracy, precision, recall, f1 = testModel(model, featureMatrixTest, labelsTest)
+    overallAccuracy += accuracy
+
+print(overallAccuracy/4)
